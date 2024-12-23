cd704af50d7762e00a39114524cdb5ec1832869d
oscarracuna <acunainfosec@proton.me>
2024-12-23 10:09:33 -0700

    Outline

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-  "fmt"
-  "net"
+	"fmt"
+	"log"
+	"net"
+  "time"
 )
+
+var (
+  ip string
+)
+
+const BLOCK_TIME = 10 * time.Minute 
+
 
 func main() {
   fmt.Println("owo")
 }
 
-func is_ip_blocked() {
-  
+func is_ip_blocked(string) bool{
+  return true
 }
 
 func block_ip() {
-
+  if is_ip_blocked(ip){
+    log.Println("%s already blocked. Skipping...", ip)
+  }
 }
 
 func unblock_ip() {
